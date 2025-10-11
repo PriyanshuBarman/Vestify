954d2c274fd61f505437f8ca5a016d3b937cb1cd
Priyanshu Barman <priyanshubarman741@gmail.com>
2025-10-11 18:09:49 +0530

    feat(backend): add upload and delete profile photo api  with Cloudinary multer integration

---
+++ Backend/src/shared/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB limit
+  },
+  fileFilter: (req, file, cb) => {
+    // Accept images only
+    if (!file.mimetype.startsWith("image/")) {
+      return cb(new Error("Only image files are allowed!"), false);
+    }
+    cb(null, true);
+  },
+});
+
+export default upload;
--- Backend/src/user/routes/profile.routes.js
+++ Backend/src/user/routes/profile.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { authenticate } from "../../shared/middlewares/auth.middleware.js";
 import * as profileController from "../controllers/profile.controller.js";
+import upload from "../../shared/middlewares/multer.middleware.js";
 
 export const profileRoutes = Router();
 
 profileRoutes.get("/:userId", authenticate, profileController.getProfile);
 profileRoutes.get("/", authenticate, profileController.searchProfile);
+
+profileRoutes.patch("/", authenticate, profileController.updateProfile);
+profileRoutes.post(
+  "/upload/avatar",
+  authenticate,
+  upload.single("avatar"),
+  profileController.uploadProfilePhoto
+);
+profileRoutes.delete(
+  "/avatar",
+  authenticate,
+  profileController.deleteProfilePhoto
+);
